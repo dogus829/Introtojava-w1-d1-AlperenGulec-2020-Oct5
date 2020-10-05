fe41573f81c9fd293867badebb5580b58a94172f
dogus829 <68568704+dogus829@users.noreply.github.com>
2020-10-05 11:15:48 -0700

    Create intro1.java
    -- Prints out the varibles of different data types in Java--

---
+++ intro1.java
@@ -0,0 +1,28 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    byte myByte = -35; // stores whole numbers from 
+	    // -128 to -127
+	    short myShort = 20000; // -32,768 to 32,767
+	    int myInt = 2000000000; // -2,147,483,648 to 2,147,483,647
+	    // "long" data type stores values between 
+	    // -2,147,483,648 to 2,147,483,647
+	    float myFloat = 2.2f;// 6 to 7 decimal digits
+	    double myDouble = 1.22345678910292;
+	    // stores fractional numbers just like
+	    // float but 15 decimal digits. 
+	    boolean myBlooean = false; 
+	    // true or false.
+	    char myChar = 'i'; // stores a single Character or 
+	    // ASCII values.
+	    
+	     System.out.println(myByte);
+	     System.out.println(myShort);
+	     System.out.println(myInt);
+	     System.out.println(myFloat);
+	     System.out.println(myDouble);
+	     System.out.println(myBlooean);
+	     System.out.println(myChar);
+	    
+	}
+}
